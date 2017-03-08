ac04fc8289edd562dfa0e38f6ca62775ab8cad2b
Onur Gürkan GÜLTEKİN <onurgurkangultekin@hotmail.com>
2017-03-08 13:28:09 +0300

    Unit Test For Mars Rover

---
+++ MarsRoverSoln/UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("96a7b8c2-0583-4ee9-8516-2f7994f7c7db")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ MarsRoverSoln/UnitTest/UnitTestMarsRover.cs
@@ -0,0 +1,98 @@
+﻿using MarsRover;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTestMarsRover
+    {
+        Plateau p = new Plateau();
+
+        /// <summary>
+        /// Constructor of unit test. Creates a plateau as min:0,0 max:5,5
+        /// </summary>
+        public UnitTestMarsRover()
+        {
+            p = new Plateau()
+            {
+                minX = 0,
+                minY = 0,
+                maxX = 5,
+                maxY = 5
+            };
+        }
+
+
+        /// <summary>
+        /// Creates first rover and makes it move and compares the result with the actual values.
+        /// </summary>
+        [TestMethod]
+        public void TestMethodFirstRover()
+        {
+            var rover = new Rover
+            {
+                X = 1,
+                Y = 2,
+                D = Direction.N
+            };
+
+            var instruction = "LMLMLMLMM";
+            foreach (var c in instruction)
+            {
+                rover.Move(c, p);
+            }
+
+            Assert.AreEqual(1, rover.X);
+            Assert.AreEqual(3, rover.Y);
+            Assert.AreEqual(Direction.N, rover.D);
+        }
+
+
+        /// <summary>
+        /// /// Creates second rover and makes it move and compares the result with the actual values.
+        /// </summary>
+        [TestMethod]
+        public void TestMethodSecondRover()
+        {
+            var rover = new Rover
+            {
+                X = 3,
+                Y = 3,
+                D = Direction.E
+            };
+
+            var instruction = "MMRMMRMRRM";
+            foreach (var c in instruction)
+            {
+                rover.Move(c, p);
+            }
+
+            Assert.AreEqual(5, rover.X);
+            Assert.AreEqual(1, rover.Y);
+            Assert.AreEqual(Direction.E, rover.D);
+        }
+
+
+        /// <summary>
+        /// create a rover on the end points of the plateau and try it move to borders. Expect to throw a user-typed exception named CantMoveException
+        /// </summary>
+        [TestMethod]
+        public void TestMethodCantMoveException()
+        {
+            var rover = new Rover
+            {
+                X = 5,
+                Y = 5,
+                D = Direction.E
+            };
+
+            var instruction = "MMMM";
+
+            foreach (var c in instruction)
+            {
+                Assert.ThrowsException<CantMoveException>(() => rover.Move(c, p));
+            }
+        }
+    }
+}
